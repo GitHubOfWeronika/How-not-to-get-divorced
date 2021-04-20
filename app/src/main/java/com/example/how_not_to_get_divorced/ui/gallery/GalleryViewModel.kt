b468af1fb254fe90d3ec040a49fcee2a547f9fcb
Marek Bauer <marekbauer07@gmail.com>
2021-04-20 18:31:53 +0200

    init project

---
+++ app/src/main/java/com/example/how_not_to_get_divorced/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.how_not_to_get_divorced.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/how_not_to_get_divorced/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.how_not_to_get_divorced.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
